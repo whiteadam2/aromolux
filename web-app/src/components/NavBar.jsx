bb271cd26e7c63df17a4d175ecde24f321956cc2
WhiteAdam <jurchenko.a@gmail.com>
2022-11-01 19:59:31 +0100

    - fix active styles in Navbar

--- web-app/src/components/NavBar.jsx
+++ web-app/src/components/NavBar.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export function NavBar() {
+  const location = useLocation();
+  const path = location.pathname + location.search;
+
   const categories = [
     { title: "Женские", url: "/products/?category=209" },
     { title: "Мужские", url: "/products/?category=212" },
   ];
   const activeItemStyle = "p-2 text-white bg-blue-400 p-2 rounded-3xl";
   const itemStyle = "px-4 py-2 text-gray-400 bg-custom-bg p-2 rounded-3xl";
+
   return (
     <ul className="my-12 flex flex-row gap-4 text-xl">
       {categories.map((category) => (
-        <li>
-          <NavLink
+        <li key={category.url}>
+          <Link
             to={category.url}
-            className={({ isActive }) =>
-              isActive ? activeItemStyle : itemStyle
-            }
+            className={path === category.url ? activeItemStyle : itemStyle}
           >
             {category.title}
-          </NavLink>
+          </Link>
         </li>
       ))}
     </ul>
