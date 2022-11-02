bfce86bf4908771d86ae1008b478b464e3703197
WhiteAdam <jurchenko.a@gmail.com>
2022-11-02 17:55:10 +0100

    - change logo

--- web-app/src/components/Header.jsx
+++ web-app/src/components/Header.jsx
@@ -6,7 +6,7 @@ export function Header({ searchValue, setSearchValue }) {
   return (
     <TelegramWrapper>
       <div className="flex justify-between items-center mb-10">
-        <img src="/images/logo-aromo.png" alt="Logo" className="h-20" />
+        <img src="/images/logo_parfum.png" alt="Logo" className="h-20" />
         <Search searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
     </TelegramWrapper>
