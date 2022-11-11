5a68ae7fd66b1e53c75d0f483de316d53a5dc1fe
WhiteAdam <jurchenko.a@gmail.com>
2022-11-11 09:22:39 +0100

    - remove BackButton

--- web-app/src/components/Cart.jsx
+++ web-app/src/components/Cart.jsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-
 import { OrdersList } from "./OrdersList";
 import { UserForm } from "./UserForm";
-import { TelegramWrapper } from "./TelegramWrapper";
-import { BackButton } from "./BackButton";
 
 export function Cart() {
   const { state: orders } = useLocation();
 
   return (
     <div className="mx-10 ">
-      <TelegramWrapper>
-        <BackButton />
-      </TelegramWrapper>
       <OrdersList orders={orders} />
       <UserForm orders={orders} />
     </div>
