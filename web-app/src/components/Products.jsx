e68abd2087a3fadddc03ecddca0b3ea9fc82d261
WhiteAdam <jurchenko.a@gmail.com>
2022-11-15 21:41:45 +0100

    - fixes

--- web-app/src/components/Products.jsx
+++ web-app/src/components/Products.jsx
@@ -33,7 +33,7 @@ export function Products() {
 
   usePrepareProducts(data);
   useOutsideClick(sortRef, () => {
-    setIsSortVisible(false);
+    dispatch(setIsSortVisible(false));
   });
 
   const total = orders.reduce(
