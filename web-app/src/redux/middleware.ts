a2fb8dccb681b62d8322365c3a5c199bd9c3efea
WhiteAdam <jurchenko.a@gmail.com>
2022-12-15 23:23:53 +0100

    - fixes

--- web-app/src/redux/middleware.ts
+++ web-app/src/redux/middleware.ts
@@ -4,9 +4,9 @@ import { cartActionTypes } from './cartSlice'
 
 export const saveCartToLocalStorage: Middleware<{}, RootState> =
   (storeApi) => (next) => (action) => {
+    next(action)
     if (cartActionTypes.includes(action.type)) {
-      next(action)
       const state = storeApi.getState()
       localStorage.setItem('cart', JSON.stringify(state.cart))
-    } else next(action)
+    }
   }
